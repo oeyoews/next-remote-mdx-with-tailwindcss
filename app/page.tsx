4358261e1b964eecb2c5218f6bd453af02910155
oeyoews <jyao4783@gmail.com>
2023-07-21 18:44:08 +0800

    fix: add missing key for listitem

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default async function App() {
       <section className="bg-neutral-100 prose mx-auto mt-16 rounded max-w-none sm:w-full md:w-1/2 p-4">
         <ul className="list-none">
           {posts.map(({ frontmatter }) => (
-            <li>
+            <li key={frontmatter.title}>
               <Link
                 href={`/posts/${frontmatter.title}`} className="capitalize"
               >
