9f42377911658a49c0c57bcf788b9b56a47d43dd
oeyoews <jyao4783@gmail.com>
2023-07-23 03:44:12 +0800

    Automatic commit

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -18,8 +18,8 @@ export default function NavBar() {
 	return (
 		// TODO: add menubar on mobile
 		// sticky
-		<header className="sticky top-0 z-50 ">
-			<nav className='prose max-w-none rounded-full my-4 w-2/3 mx-auto px-4 py-1 justify-end flex items-center print:hidden space-x-2'>
+		<header className="sticky top-0 z-50">
+			<nav className='prose max-w-none rounded-full my-4 lg:w-2/3 mx-auto px-4 py-1 justify-end flex items-center print:hidden space-x-2'>
 				{isHomePage ? null : Home}
 				{GithubLink}
 				{AboutPage}
