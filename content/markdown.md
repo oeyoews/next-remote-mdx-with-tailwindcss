29f39f888c20291a6a59ba2e65de6c9b9986b027
oeyoews <jyao4783@gmail.com>
2023-07-25 21:37:38 +0800

    feat: better image

--- content/markdown.md
+++ content/markdown.md
@@ -4,6 +4,7 @@ date: '2023/07/23'
 slug: oeyoews-hello
 description: 'Markdown file'
 cover: '/banner02.avif'
+password: '123'
 ---
 
 > markdown test on mdx
--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const domains = [
+  'cdn-icons-png.flaticon.com',
   'unsplash.com',
   'plus.unsplash.com',
   'images.unsplash.com',
