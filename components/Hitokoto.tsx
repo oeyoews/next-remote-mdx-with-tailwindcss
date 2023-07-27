1d410945de698421b0a1ef6842da140792c9d1e4
oeyoews <jyao4783@gmail.com>
2023-07-28 01:38:10 +0800

    Automatic commit

--- components/Hitokoto.tsx
+++ components/Hitokoto.tsx
@@ -1,12 +1,18 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function Hitokoto() {
   const [hitokoto, setHitokoto] = useState('');
 
   async function fetchHitokoto() {
-    const response = await fetch('https://v1.hitokoto.cn');
+    const response = await fetch('https://v1.hitokoto.cn', {
+      cache: 'default',
+      next: {
+        // not work on this instance
+        revalidate: 5,
+      },
+    });
     const { hitokoto: hitokotoText } = await response.json();
     setHitokoto(hitokotoText);
   }
@@ -16,7 +22,11 @@ function Hitokoto() {
   }, []);
 
   return (
-    <div className="text-sm mt-8 text-center bg-gradient-to-r from-red-300 via-purple-400 to-blue-500 rounded-sm p-1 bg-clip-text text-transparent select-none">
+    <div
+      className="text-sm mt-8 text-center bg-gradient-to-r from-red-300 via-purple-400 to-blue-500 rounded-sm p-1 bg-clip-text text-transparent select-none hover:cursor-pointer"
+      title="点击刷新一言"
+      onClick={fetchHitokoto}
+    >
       {hitokoto}
     </div>
   );
