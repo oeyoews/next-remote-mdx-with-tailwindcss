9914d0a91155b3cd143c8d9cd57af29acdca20ff
oeyoews <jyao4783@gmail.com>
2023-07-29 17:48:32 +0800

    Automatic commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,52 +1,76 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ['class'],
   content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
   ],
   theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
     extend: {
-      typography: ({ theme }) => ({
-        DEFAULT: {
-          css: {
-            // '--tw-prose-body': theme('colors.text'),
-            // '--tw-prose-headings': theme('colors.rose'),
-            // '--tw-prose-links': theme('colors.iris'),
-            // '--tw-prose-bold': theme('colors.rose'),
-            // '--tw-prose-counters': theme('colors.rose'),
-            // '--tw-prose-bullets': theme('colors.rose'),
-            // '--tw-prose-hr': theme('colors.gold'),
-            // '--tw-prose-quotes': theme('colors.foam'),
-            // '--tw-prose-pre-bg': 'rgb(38 35 58)',
-            // '--tw-prose-quote-borders': theme('colors.pine'),
-            // '--tw-prose-captions': theme('colors.subtle'),
-            // '--tw-prose-th-borders': theme('colors.muted'),
-            // '--tw-prose-td-borders': theme('colors.muted'),
-            h1: {
-              marginTop: '1.3em',
-            },
-            img: {
-              margin: 'auto',
-              borderRadius: theme('borderRadius.lg'),
-            },
-            // code: {
-            //   color: theme("colors.pine"),
-            //   background: "rgb(38 35 58)",
-            //   padding: "0.25rem 0.4rem",
-            //   borderRadius: "0.25rem",
-            //   fontWeight: "600",
-            // },
-            'code::before': {
-              content: '',
-            },
-            'code::after': {
-              content: '',
-            },
-          },
+      colors: {
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))',
         },
-      }),
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))',
+        },
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))',
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))',
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))',
+        },
+        popover: {
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))',
+        },
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))',
+        },
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
+      keyframes: {
+        'accordion-down': {
+          from: { height: 0 },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
     },
   },
-  plugins: [require('@tailwindcss/typography')],
+  plugins: [require('tailwindcss-animate'), require('@tailwindcss/typography')],
 };
