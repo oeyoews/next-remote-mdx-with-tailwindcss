5384331e320980892f65c4c66e18a5809253d3a5
oeyoews <jyao4783@gmail.com>
2023-07-31 02:54:33 +0800

    perf: remove TransitionWrapper

--- components/MdxComponents.tsx
+++ components/MdxComponents.tsx
@@ -1,14 +1,7 @@
-import { FaGithub } from 'react-icons/fa';
-
 import Pre from './CopyCode';
-
-function Star() {
-  return <FaGithub className={`h-12 w-12 text-cyan-300`} />;
-}
 
 const mdxCustomComponents = {
   pre: Pre,
-  Star,
 };
 
 export default mdxCustomComponents;
