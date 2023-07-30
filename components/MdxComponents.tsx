899708b0a82c77c1b017e5c8c36b3c159f6ecd71
oeyoews <jyao4783@gmail.com>
2023-07-31 02:51:39 +0800

    perf: remove medium-zoom

--- components/MdxComponents.tsx
+++ components/MdxComponents.tsx
@@ -1,7 +1,6 @@
 import { FaGithub } from 'react-icons/fa';
 
 import Pre from './CopyCode';
-import { ImageZoom as Image } from './ImageZoom';
 
 function Star() {
   return <FaGithub className={`h-12 w-12 text-cyan-300`} />;
@@ -10,7 +9,6 @@ function Star() {
 const mdxCustomComponents = {
   pre: Pre,
   Star,
-  Image,
 };
 
 export default mdxCustomComponents;
